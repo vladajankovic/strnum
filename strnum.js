20724d90f22d7686fde3769b05e8242ddbddeec3
vladajankovic <vladajankovic1999@gmail.com>
2024-09-22 01:20:09 +0200

    Update test

--- strnum.js
+++ strnum.js
@@ -30,8 +30,7 @@ const defaultOptions = {
     removeLeadingZeros: true,
     removePositiveSign: true,
     eNotation: true,
-    skipLike: undefined,
-    leadingZeros : true
+    skipLike: undefined
 };
 
 function toNumber(str, options = {}){
@@ -78,98 +77,9 @@ function toNumber(str, options = {}){
         if (num != int_num.toString()) return num;
         return int_num;      
     }
-    // else{
-    //     //separate negative sign, leading zeros, and rest number
-    //     const match = numRegex.exec(num);
-    //     if(match){
-    //         const sign = match[1];
-    //         const leadingZeros = match[2];
-    //         let numTrimmedByZeros = trimZeros(match[3]); //complete num without leading zeros
-    //         //trim ending zeros for floating number
-            
-    //         const eNotation = match[4] || match[6];
-    //         if(!options.leadingZeros && leadingZeros.length > 0 && sign && num[2] !== ".") return str; //-0123
-    //         else if(!options.leadingZeros && leadingZeros.length > 0 && !sign && num[1] !== ".") return str; //0123
-    //         else{//no leading zeros or leading zeros are allowed
-    //             const num = Number(num);
-    //             const numStr = "" + num;
-    //             if(numStr.search(/[eE]/) !== -1){ //given number is long and parsed to eNotation
-    //                 if(options.eNotation) return num;
-    //                 else return str;
-    //             }else if(eNotation){ //given number has enotation
-    //                 if(options.eNotation) return num;
-    //                 else return str;
-    //             }else if(num.indexOf(".") !== -1){ //floating number
-    //                 // const decimalPart = match[5].substr(1);
-    //                 // const intPart = trimmedStr.substr(0,trimmedStr.indexOf("."));
-
-                    
-    //                 // const p = numStr.indexOf(".");
-    //                 // const givenIntPart = numStr.substr(0,p);
-    //                 // const givenDecPart = numStr.substr(p+1);
-    //                 if(numStr === "0" && (numTrimmedByZeros === "") ) return num; //0.0
-    //                 else if(numStr === numTrimmedByZeros) return num; //0.456. 0.79000
-    //                 else if( sign && numStr === "-"+numTrimmedByZeros) return num;
-    //                 else return str;
-    //             }
-                
-    //             if(leadingZeros){
-    //                 // if(numTrimmedByZeros === numStr){
-    //                 //     if(options.leadingZeros) return num;
-    //                 //     else return str;
-    //                 // }else return str;
-    //                 if(numTrimmedByZeros === numStr) return num;
-    //                 else if(sign+numTrimmedByZeros === numStr) return num;
-    //                 else return str;
-    //             }
-
-    //             if(num === numStr) return num;
-    //             else if(num === sign+numStr) return num;
-    //             // else{
-    //             //     //number with +/- sign
-    //             //     trimmedStr.test(/[-+][0-9]);
-
-    //             // }
-    //             return str;
-    //         }
-    //         // else if(!eNotation && trimmedStr && trimmedStr !== Number(trimmedStr) ) return str;
-            
-    //     }else{ //non-numeric string
-    //         return str;
-    //     }
-    // }
+    
+    // not a valid number
     return str;
 }
 
-/**
- * 
- * @param {string} numStr without leading zeros
- * @returns 
- */
-function trimZeros(numStr){
-    if(numStr && numStr.indexOf(".") !== -1){//float
-        numStr = numStr.replace(/0+$/, ""); //remove ending zeros
-        if(numStr === ".")  numStr = "0";
-        else if(numStr[0] === ".")  numStr = "0"+numStr;
-        else if(numStr[numStr.length-1] === ".")  numStr = numStr.substr(0,numStr.length-1);
-        return numStr;
-    }
-    return numStr;
-}
 module.exports = toNumber
-
-// let x = "+00011111111111111111111"
-// console.log(x)
-// console.log(toNumber(x))
-// console.log(toNumber(" 06", {removeLeadingZeros: false, removePositiveSign: false}));
-// console.log(toNumber("+06", {removeLeadingZeros: false, removePositiveSign: false}));
-// console.log(toNumber("-06", {removeLeadingZeros: false, removePositiveSign: false}));
-// console.log(toNumber(" 06", {removeLeadingZeros: false, removePositiveSign: true}));
-// console.log(toNumber("+06", {removeLeadingZeros: false, removePositiveSign: true}));
-// console.log(toNumber("-06", {removeLeadingZeros: false, removePositiveSign: true}));
-// console.log(toNumber(" 06", {removeLeadingZeros: true, removePositiveSign: false}));
-// console.log(toNumber("+06", {removeLeadingZeros: true, removePositiveSign: false}));
-// console.log(toNumber("-06", {removeLeadingZeros: true, removePositiveSign: false}));
-// console.log(toNumber(" 06", {removeLeadingZeros: true, removePositiveSign: true}));
-// console.log(toNumber("+06", {removeLeadingZeros: true, removePositiveSign: true}));
-// console.log(toNumber("-06", {removeLeadingZeros: true, removePositiveSign: true}));
